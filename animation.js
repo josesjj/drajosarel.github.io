45e869c73e2558f73770675c4a45806c64ea80fd
josesjj <153836552+josesjj@users.noreply.github.com>
2025-08-19 17:38:55 -0400

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,8 @@
+  // Animación de entrada para todas las secciones con fade-in
+
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+      setTimeout(() => el.classList.add('visible'), 200);
+    });
+  });
